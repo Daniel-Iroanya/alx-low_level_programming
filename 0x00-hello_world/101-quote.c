66709945360d31c9833db37da7c69bf975c458aa
Daniel-Iroanya <danieliroanya08@gmail.com>
2022-07-04 19:33:14 -0700

    c-6

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,12 @@
 /**
  * main - Entry point
  *
- * return: Always 1 (success)
+ * Return: 1 is always returned
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return(1);
+	write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		59);
+	return (1);
 }
