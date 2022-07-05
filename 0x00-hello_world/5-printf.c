954288c42b573a9fb1b0416c58ffa3b478f67bca
Daniel-Iroanya <danieliroanya08@gmail.com>
2022-07-04 19:15:08 -0700

    c-5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * return: Always 0 (success)
+ * Return: 0 signifies everything was ok
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
