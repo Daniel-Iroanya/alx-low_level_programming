99a0882c25b0454c2f14eb840d331cb5966dcfc5
Daniel-Iroanya <danieliroanya08@gmail.com>
2022-07-07 21:08:44 -0700

    task 3 hot fix 5

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,15 +8,14 @@
 int main(void)
 {
 	int a;
-	int A;
 
 	for (a = 97 ; a <= 122 ; a++);
 	{
 	putchar(a);
 	}
-	for (A = 65 ; A <= 90 ; A++);
+	for (a = 65 ; A <= 90 ; A++);
 	{
-	putchar(A);
+	putchar(a);
 	}
 	putchar(10);
 	return (0);
