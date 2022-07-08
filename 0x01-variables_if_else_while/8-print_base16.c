4e144cf7d7fc0301c379eb4109c3af4b19c85d17
Daniel-Iroanya <danieliroanya08@gmail.com>
2022-07-08 00:44:35 -0700

    task 8 correction

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,17 +5,18 @@
  *
  * Return: 0
  */
-int main(0)
+int main(void)
 {
 	char ch;
 	int n;
+
 	for (n = 48 ; n <= 57 ; n++)
 	{
 		putchar(n);
-	}	
+	}
 	for (ch = 'a' ; ch <= 'f' ; ch++)
 	{
-		puchar(ch);
+		putchar(ch);
 	}
 	putchar(10);
 	return (0);
