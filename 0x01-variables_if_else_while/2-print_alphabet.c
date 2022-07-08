a612967f0799d4c855f1076e790a1a51a6c7abf1
Daniel-Iroanya <danieliroanya08@gmail.com>
2022-07-07 19:47:35 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 always
+ */
+int main(void)
+{
+	int ch;
+
+	
+	for(ch=97;ch<=122;ch+=)
+	putchar(ch);
+	putchar(10);
+	return (0);
+}
