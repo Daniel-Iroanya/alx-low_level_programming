e56c4f3c7cbc234f36e382f80b236509745f3984
Daniel-Iroanya <danieliroanya08@gmail.com>
2022-07-08 00:34:10 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(0)
+{
+	char ch;
+	int n;
+	for (n = 48 ; n <= 57 ; n++)
+	{
+		putchar(n);
+	}	
+	for (ch = 'a' ; ch <= 'f' ; ch++)
+	{
+		puchar(ch);
+	}
+	putchar(10);
+	return (0);
+}
