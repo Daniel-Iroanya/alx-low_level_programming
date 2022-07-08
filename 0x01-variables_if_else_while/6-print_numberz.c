3b481e6f6d2731062088af28606370ebdfa4b3b6
Daniel-Iroanya <danieliroanya08@gmail.com>
2022-07-07 23:29:16 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48 ; a < 57 ; a++)
+	{
+		putchar(a);
+	}
+	putchar(10);
+	return (0);
+}
