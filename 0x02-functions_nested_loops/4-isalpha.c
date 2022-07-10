b22549906a6e90ed70fef99deb0ee9ee16c75e9a
Daniel-Iroanya <danieliroanya08@gmail.com>
2022-07-09 23:06:34 -0700

    hot fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _isalpha -> checks for lower or upper case letter
- * 
+ * _isalpha -> checks for lower or upper case letter.
+ *
  * @c: arguement receives character type
  *
  * Return: 1 or 0 based on condition
