2c4dd2afe83bd5699db93ef10a3f51046025a4e7
Daniel-Iroanya <danieliroanya08@gmail.com>
2022-07-09 22:57:51 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha -> checks for lower or upper case letter
+ * 
+ * @c: arguement receives character type
+ *
+ * Return: 1 or 0 based on condition
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
