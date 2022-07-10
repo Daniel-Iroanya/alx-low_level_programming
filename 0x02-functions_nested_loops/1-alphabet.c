a8a63aa129faae277e3728505a77e0a2aa4dcac1
Daniel-Iroanya <danieliroanya08@gmail.com>
2022-07-09 20:53:38 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - prints lower case letters followed by new line
+ */
+void print_alphabet(void)
+{
+	for(j = 'a' ; j <= 'z' ; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
