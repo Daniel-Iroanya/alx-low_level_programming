ab963f90c1a2aa82208b02d6c30330c188acadfb
Daniel-Iroanya <danieliroanya08@gmail.com>
2022-07-13 21:46:38 -0700

    hot fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,14 +7,14 @@
 void rev_string(char *s)
 {
 	char tmp;
-	int i, lenght1, length2;
+	int i, length1, length2;
 
 	length1 = 0;
 	length2 = 0;
 
 	while (s[length1] != '\0')
 		length1++;
-	length2 = length - 1;
+	length2 = length1 - 1;
 	for (i = 0; i < length1 / 2; i++)
 	{
 		tmp = s[i];
