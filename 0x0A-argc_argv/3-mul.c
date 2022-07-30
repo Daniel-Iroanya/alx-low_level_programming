843469e525f7b17b72c01700320ddba99a0503c7
Daniel-Iroanya <danieliroanya08@gmail.com>
2022-07-30 05:10:28 -0700

    task 3 hot fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 /**
- * main -> function to multiply argumengts
- * @argc: argument count of arguments passed 
- * @argv: argument vector of arguments passed
- * Return: 0 successful
- */
+* main -> function to multiply argumengts
+* @argc: argument count of arguments passed 
+* @argv: argument vector of arguments passed
+* Return: 0 successful
+*/
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
+	int result,num1, num2;
 
 	if (argc != 3)
 	{
