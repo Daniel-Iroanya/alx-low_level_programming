de5b627505880374912000b029b74fe65d6451a8
Daniel-Iroanya <danieliroanya08@gmail.com>
2022-07-30 05:01:44 -0700

    hot fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,6 +10,7 @@
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
+
 	if (argc != 3)
 	{
 		printf("%s\n", "Error");
@@ -18,6 +19,7 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 	result = num1 * num2;
+	
 	printf("%d\n", result);
 	return (0); 
 }
