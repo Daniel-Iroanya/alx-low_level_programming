fbffe091740237803aa5fb3aeb2c9aff7b61af95
Daniel-Iroanya <danieliroanya08@gmail.com>
2022-07-30 03:30:18 -0700

    hot fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
 * @argv: an array of strings containing the arguments
 * Return: 0 means successful
 */
-int main (int argc, char *argv[])
+int main (int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
