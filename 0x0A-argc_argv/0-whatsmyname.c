888998df4edf5893d039c849b7f4394f03af35be
Daniel-Iroanya <danieliroanya08@gmail.com>
2022-07-30 03:23:54 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main -> this is a function to print its name
+* @argc: the number of commandline arguments
+* @argv: an array of strings containing the arguments
+* Return: 0 means successful
+*/
+int main (int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
