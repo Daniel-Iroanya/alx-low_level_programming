16879783c6dc5fb2180600615befdae2443fad97
Daniel-Iroanya <danieliroanya08@gmail.com>
2022-07-30 04:47:22 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -> function to multiply argumengts
+ * @argc: argument count 
+ * @argv: argument vector
+ * Return: 0 successful
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0); 
+}
